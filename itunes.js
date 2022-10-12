ed56ad157bb298be454104a81deaeab3d0748fc4
kevin <kkevinlunaa95@gmail.com>
2022-10-12 14:18:21 +0200

    Func chargeNewSong Ok

--- itunes.js
+++ itunes.js
@@ -1,19 +1,27 @@
 //"use strict";
 
-const chargeSongs = async() =>{
+const chargeNewSongs = async() =>{
     try{
     const respuesta = await fetch("https://itunes.apple.com/search?term=galantis&media=music");        
     const resultado = await respuesta.json();
-    let release_date = "";
-    for (let i = 0; i < resultado.results.length; i++) {
-        release_date = (resultado.results[i].releaseDate);
+    let release_date = resultado.results.releaseDate;
+    let dataObject = resultado.results;
+        dataObject.sort( (a,b) =>{
+            if(a.releaseDate == b.releaseDate){
+                return 0;
+            }
+            if(a.releaseDate < b.releaseDate){
+                return 1;
+            }
+            return -1;
+        });
+    for (let i = 0; i < 10; i++) {
+        release_date = (dataObject[i].releaseDate);
+        console.log(dataObject[i].trackName)
     }
-    console.log(release_date);
-
-
     } catch (e){
         console.log(e)
     }
 }
 
-chargeSongs();
+chargeNewSongs();
